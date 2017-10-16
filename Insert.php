43045639d2d36029ba7229a9d8be3132c203df0d
Jaime Teixidó <elteixi97rgmail.com>
2017-10-16 12:54:32 +0200

    Insert Con accionas, funciones y code

--- Insert.php
+++ Insert.php
@@ -1,9 +1,24 @@
 <?php
-require("Conexiondb.php");
-$nombre = $_GET['nombre'];
-$apellido = $_GET['apellido'];
+$code = $_GET['code'];
+$action = $_GET['action'];
 
-$query = "INSERT INTO datos (Nombre, Apellido) VALUES ('$nombre','$apellido')";
-mysqli_query($conexion, $query) or die(mysqli_error());
-mysqli_close($conexion);
+switch ($action) {
+    case 'InsertinDatos':
+        InsertinDatos();
+        break;
+    
+    default:
+        echo 'Error, action no valid.';
+        break;
+}
+
+function InsertinDatos(){
+require("Conexiondb.php");
+    $name = $_GET['name'];
+    $lastname = $_GET['lastname'];
+    $query = "INSERT INTO datos (Nombre, Apellido) VALUES ('$name','$lastname')";
+    mysqli_query($conexion, $query) or die(mysqli_error());
+    mysqli_close($conexion);
+}
+
 ?>
